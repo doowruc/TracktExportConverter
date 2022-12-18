3786559d25b29b9fd3577fbbfe643bf623ee68bf
doowruc <doowruc@doowruc.dev>
2022-12-18 00:32:29 +0000

    highlight active nav

---
+++ TraktExportConverter/Services/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace TraktExportConverter.Services
+{
+    public static class NavItems
+    {
+        public const string Index = "Index";
+        public const string Watched = "Watched";
+    }
+}
---
+++ TraktExportConverter/Services/Extensions.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace TraktExportConverter.Services
+{
+    public static class Extensions
+    {
+        public static string NavLinkTextClass(this ViewContext viewContext, string value) =>
+            Path.GetFileNameWithoutExtension(viewContext.ActionDescriptor.DisplayName) == value ? "active fw-bold" : "";
+    }
+}
