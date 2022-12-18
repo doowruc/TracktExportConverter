4bb14ef4620a302df80f5298504ee108d2706d7d
doowruc <doowruc@doowruc.dev>
2022-12-18 00:18:40 +0000

    Merge Watched Shows & Movies into a single page

--- TraktExportConverter/wwwroot/js/watched.js
+++ TraktExportConverter/wwwroot/js/watched.js
@@ -1,30 +1,76 @@
+var watchedTable;
+
+var clearForm = () => {
+    $("#import-type").val("");
+    $("#import-file").val("");
+}
+
 $("#import-file").change(function (e) {
     const files = e.target.files;
     if (files.length === 0) {
-        $("#import-file").val("");
+        clearForm();
         return;
     }
     if (window.FormData === undefined) {
         errorAlert("This browser doesn't support HTML5 file uploads");
-        $("#import-file").val("");
+        clearForm();
         return;
     }
     $("#import-form").submit();
 });
 
-var initializeTable = (table, columns, sort, importUrl) => {
-    var dataTable = table.DataTable({
+$("#import-form").submit(function (e) {
+    e.preventDefault();
+    const formData = new FormData($(this)[0]);
+    console.log(formData);
+    $.ajax({
+        url: "/Watched/Import",
+        type: "POST",
+        data: formData,
+        contentType: false,
+        processData: false,
+        success: function (data) {
+            if (data.success) {
+                watchedTable.clear().rows.add(data.data).draw();
+            }
+            else {
+                errorAlert(data.error);
+            }
+        }
+    });
+});
+
+var startImport = type => {
+    $("#import-type").val(type);
+    $("#import-file").trigger("click");
+}
+
+$(function () {
+    watchedTable = $("#watched-table").DataTable({
         dom: "<'row mb-3'<'col-12 text-end'B>>" +
             "<'row'<'col-6'l><'col-6'f>>" +
             "<'row'<'col-12'tr>>" +
             "<'row'<'col-5'i><'col-7'p>>",
         buttons: [
             {
+                extend: "collection",
                 text: "Import",
                 className: "page-link",
-                action: function () {
-                    $("#import-file").trigger("click");
-                }
+                align: "button-right",
+                buttons: [
+                    {
+                        text: "Shows",
+                        action: function () {
+                            startImport("shows");
+                        }
+                    },
+                    {
+                        text: "Movies",
+                        action: function () {
+                            startImport("movies");
+                        }
+                    }
+                ]
             },
             {
                 extend: "collection",
@@ -42,35 +88,26 @@ var initializeTable = (table, columns, sort, importUrl) => {
                 ]
             }
         ],
-        columns: columns,
+        columns: [
+            {
+                data: "title",
+                width: "50%"
+            },
+            {
+                data: {
+                    "_": "playDate",
+                    display: "playDateDisplay"
+                },
+                width: "50%",
+                class: "text-end"
+            }
+        ],
         order: [
-            [sort, "desc"]
+            [1, "desc"]
         ]
     });
 
-    dataTable.on("draw", function () {
+    watchedTable.on("draw", function () {
         formatTime();
     });
-
-    $("#import-form").on("submit", function (e) {
-        e.preventDefault();
-        const formData = new FormData($(this)[0]);
-        $.ajax({
-            url: importUrl,
-            type: "POST",
-            data: formData,
-            contentType: false,
-            processData: false,
-            success: function (data) {
-                if (data.success) {
-                    dataTable.clear().rows.add(data.data).draw();
-                }
-                else {
-                    errorAlert(data.error);
-                }
-            }
-        });
-    });
-
-    return dataTable;
-};
+});
